dbc2e82469dedadbe32edecbb8066ee6a1cbf0fc
Victorscool <94493104+VictorScoolExper@users.noreply.github.com>
2025-07-10 11:08:12 -0700

    Docker Copied App to Container
    Dockerfile/Podman config , copied entire app to container

--- Ai Influencer Project/manager-portal/backend-flask/migrate.py
+++ Ai Influencer Project/manager-portal/backend-flask/migrate.py
@@ -1,9 +1,9 @@
 import sqlite3
 import os
-### NEEED TO EDDIT THIS FILE TO RUN MIGRATIONS
+### Runs in Terminal will be used later for somethime for migration.
 def run_migrations():
     # Connect to the database
-    db_path = os.path.join(os.path.dirname(__file__), 'word_bank.db')
+    db_path = os.path.join(os.path.dirname(__file__), 'something_DB_GOES_HERE.db')
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     
